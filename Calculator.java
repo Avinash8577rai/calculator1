f048ff835b1daf6ebd014be9756cc6152a4c04bb
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-01-07 13:10:46 +0530

    first

---
+++ Calculator.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Calculator{
+	public static void main(String[] agrs){
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enetr nuber for addition");
+		double a = scanner.nextDouble();
+		double b = scanner.nextDouble();
+		System.out.println(a+b);
+}}
