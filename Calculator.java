f6543cc5a967886d6b5f74115f4a61f25ad7b59f
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-01-07 13:29:49 +0530

    substract added

--- Calculator.java
+++ Calculator.java
@@ -3,11 +3,14 @@ class Calculator{
 	public static int add(int a,int b){
 		return a+b;
 	}
+	public static int subtract(int a,int b){
+		return a-b;
+	}
 	public static void main(String[] agrs){
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enetr nuber for addition");
 		double a = scanner.nextDouble();
 		double b = scanner.nextDouble();
 		
-		System.out.println(add(a,b));
+		System.out.println(subtract(a,b));
 }}
