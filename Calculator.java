62cd7df00daba7f037323a74886fab2bc8604c84
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-01-07 15:36:31 +0530

    substract

--- Calculator.java
+++ Calculator.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 class Calculator{
-		public static int subtract(int a,int b){
+		public static int substract(int a,int b){
 		return a-b;
-	}
+	}	}
 	public static void main(String[] agrs){
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enetr nuber for addition");
-		double a = scanner.nextDouble();
-		double b = scanner.nextDouble();
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
 		
-		System.out.println(subtract(a,b));
+		System.out.println(substract(a,b));
 }}
