aeb274436d038966b9d0599a94fe37c38b7bf2f5
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-01-07 13:20:48 +0530

    (addition)

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;
 class Calculator{
+	public static int add(int a,int b){
+		return a+b;
+	}
 	public static void main(String[] agrs){
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enetr nuber for addition");
 		double a = scanner.nextDouble();
 		double b = scanner.nextDouble();
-		System.out.println(a+b);
+		
+		System.out.println(add(a,b));
 }}
